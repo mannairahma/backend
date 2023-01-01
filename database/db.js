470dfca5a43210b180ebdb33ccbffeea1f4e73a8
mannai <mannai@isikef.u-jendouba.tn>
2023-01-01 17:05:11 +0100

    connect backend to cosmos db

--- database/db.js
+++ database/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     //db: 'mongodb://localhost:27017/reactdb'//
-    db: 'mongodb+srv://rahma:rahma@cluster0.cvsswis.mongodb.net/?retryWrites=true&w=majority'
+    //db: 'mongodb+srv://rahma:rahma@cluster0.cvsswis.mongodb.net/?retryWrites=true&w=majority'
+    db: 'mongodb://pfemaster-sms:0Nedyb0gTZde0EvvcfbYIm8DPhEp91zg7AK5KVp4iKctyH3daF86iBqcSWbrkryBAznDhEIViauhACDbe0yDMA==@pfemaster-sms.mongo.cosmos.azure.com:10255/smsDB?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@pfemaster-sms@'
   };
